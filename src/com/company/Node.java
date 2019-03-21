e6128304e5be45d6d29313e01abba138578df21e
GogoWYJ <yzw0060@gmail.com>
2019-03-20 23:32:01 -0500

    first

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.concurrent.ConcurrentSkipListSet;
+
 public class Node {
+
+    private int nodeID;
+    private String hostName;
+    private int port;
+    private double x, y;
+    private ConcurrentSkipListSet<Integer> links;
+
+    public Node(int nodeID, String hostName, int port, double x, double y) {
+        this.nodeID = nodeID;
+        this.hostName = hostName;
+        this.port = port;
+        this.x = x;
+        this.y = y;
+        this.links = new ConcurrentSkipListSet<Integer>();
+    }
+
+    
+
 }
